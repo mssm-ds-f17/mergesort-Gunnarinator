fcbdc015626086cd904280ae839d79df1b07cb61
Gunnarinator <gunnar.eastman@gmail.com>
2017-09-14 20:36:10 -0400

    Add files via upload
    It works now!!!

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ class Thing{
 public:
     int id;
     int thingnyum;
-  //  Thing();
+    //  Thing();
     Thing(int i = 0);
 };
 /*
@@ -41,7 +41,7 @@ bool comparethings(const Thing& a, const Thing& b){
     return a.id<b.id;
 }
 
-bool isSorted(const vector<Thing>& v, function<bool(const Thing& a, const Thing&b)> comp){
+bool isSorted(const vector<Thing>& v, function<bool(const Thing& a, const Thing&b)> comparethings){
     for(unsigned int i = 0; i<v.size()-1; ++i){
         if(v[i]>v[i+1]){
             return false;
@@ -50,38 +50,86 @@ bool isSorted(const vector<Thing>& v, function<bool(const Thing& a, const Thing&
     return true;
 }
 
+void printV(vector<int> v){
+    for(int i = 0; i<v.size(); ++i){
+        cout << v[i] << ", ";
+    }
+    cout << endl;
+}
+
 //, function<bool(const Thing& a, const Thing&b)> comp
 
-void Sort(vector<int>& v){
-    int size = v.size();
-    vector<int> a(size/2);
-    vector<int> b(size/2);
-    if(size>1){
-    for(unsigned int i = 0; i<size/2; ++i){
-        a[i] = v[i];
+void Sort(vector<Thing>& v, function<bool(const Thing& a, const Thing&b)> comparethings)
+          {
+    if(v.size()==1 || v.size() == 0){
+        return;
     }
-    for(unsigned int i = (size/2)+1; i<size; ++i){
-        b[i] = v[i];
+    vector<Thing> a;
+    vector<Thing> b;
+
+    for(unsigned int i = 0; i<v.size(); ++i){
+        if(i<(v.size()/2)) {
+            a.push_back(v[i]);
+        } else {
+            b.push_back(v[i]);
+        }
     }
-    Sort(a);
-    Sort(b);
-    }
-    unsigned int vindex = 0;
+    Sort(a, comparethings);
+    Sort(b, comparethings);
     unsigned int aindex = 0;
     unsigned int bindex = 0;
-    while(vindex<=size-1 || aindex<=a.size()-1 || bindex<=b.size()-1){
-            if(comparethings(a[aindex], b[bindex]) == true && aindex<a.size()){
-                v[vindex] = a[aindex];
-                ++aindex;
-                ++vindex;
+    for(unsigned int vi = 0; vi < v.size(); ++vi)
+    {
+        if(aindex<a.size()){
+            if(bindex<b.size()){
+                if(comparethings(a[aindex], b[bindex])){
+                    v[vi].id = a[aindex].id;
+                    v[vi].thingnyum = v[vi].thingnyum;
+                    ++aindex;
+                }
+                else{
+                    v[vi]=b[bindex];
+                    ++bindex;
+                }
+            } else {
+                for(int j = vi; j<v.size(); ++j){
+                    cout << aindex   << "aindex" << endl;
+                    v[j].id = a[aindex].id;
+                    v[j].thingnyum = a[aindex].thingnyum;
+                    ++aindex;
+                }
             }
-            if(comparethings(b[bindex], a[aindex]) == true && bindex<b.size()){
-                v[vindex] = b[bindex];
-                ++bindex;
-                ++vindex;
+        }  else {
+            if(bindex<b.size()){
+                for(int i = vi; i<v.size(); ++i){
+                    v[i].id = b[bindex].id;
+                    v[i].thingnyum = b[bindex].thingnyum;
+                    ++bindex;
+
+                }
             }
         }
+        /*if(bindex<b.size()){
+            if(aindex<a.size()){
+                if(b[bindex]<a[aindex]){
+                    v[vi] = b[bindex];
+                    ++bindex;
+                }
+            }
+        } else {
+            if(aindex<a.size()){
+                for(int j = vi; j<v.size(); ++j){
+                    cout<< aindex   << "aindex" << endl;
+                    v[j] = a[aindex];
+                    ++aindex;
+
+                }
+            }
+      }*/
+    }
+    return;
 }
+
 
 vector<Thing> makerandom(int count, int max){
     vector<Thing> retval;
@@ -96,7 +144,6 @@ vector<Thing> makerandom(int count, int max){
 
 // assuming sorted:
 bool isStable(vector<Thing> vec){
-    int system;
     for(int i = 0; i<vec.size()-1; ++i){
         if(vec[i].id == vec[i+1].id){
             if(vec[i].thingnyum > vec[i+1].thingnyum){
@@ -115,13 +162,9 @@ void printVector(vector<Thing> values){
 
 
 TEST(SortTest, isSortedVectorSorted){
-    vector<Thing> vec = makerandom(5, 101);
-    printVector(vec);
-    vector<Thing> v = {1, 2, 3, 3, 4};
-  //  ASSERT_TRUE(isSorted(v, comparethings));
-    vector<int> test = {4, 21, 13, 5, 6, 2};
-    Sort(test);
-    ASSERT_TRUE(isSorted(test));
+    vector<Thing> test = {23, 21, 13, 5, 7, 2, 5, 3 ,9, 20, 32, 2, 5, 7};
+    Sort(test, comparethings);
+    ASSERT_TRUE(isSorted(test, comparethings));
 }
 
 int main(int argc, char **argv)
